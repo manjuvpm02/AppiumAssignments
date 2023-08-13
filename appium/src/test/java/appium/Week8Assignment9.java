2d0da7a1b7d439549eb9a5062ab587c60989a3ec
manjuvpm02 <manjuvpm12@gmail.com>
2023-08-14 01:14:51 +0530

    Week8Assignment9.java
    Wee8 assignment 9

---
+++ appium/src/test/java/appium/Week8Assignment9.java
@@ -0,0 +1,41 @@
+package appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class Week8Assignment9 {
+	
+@SuppressWarnings("deprecation")
+@Test
+	
+	public void closeApp() throws MalformedURLException
+	{
+		//Defining appium server URL
+		String urlOfAppiumServer = "http://127.0.0.1:4723/";
+		URL appiumServerUrl = new URL(urlOfAppiumServer);
+		UiAutomator2Options options=new UiAutomator2Options();
+		//Setting the device name
+		options.setDeviceName("Pixel2"); 
+		//Setting app location
+		options.setApp("/Users/apple/eclipse-workspace/MDITTrainings/MDIT2023/appium/src/test/resources/ApiDemos-debug.apk");
+		
+		//Launching the app
+		AndroidDriver driver=new AndroidDriver(appiumServerUrl,options);
+		//Implicitly waiting for 10 seconds for tester to see the app launch
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//Closing app
+		driver.closeApp();
+
+		//Quitting the driver
+		driver.quit();
+		
+	}
+
+}
