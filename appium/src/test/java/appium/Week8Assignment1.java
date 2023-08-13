1cd76e10985696f2c04d23fda59202b1715c62f5
manjuvpm02 <manjuvpm12@gmail.com>
2023-08-14 00:28:12 +0530

    Update Week8Assignment1.java

--- appium/src/test/java/appium/Week8Assignment1.java
+++ appium/src/test/java/appium/Week8Assignment1.java
@@ -14,15 +14,20 @@ public class Week8Assignment1 {
 	@Test
 	public void firstTest() throws MalformedURLException
 	{
-		
+		//Defining appium server URL
 		String urlOfAppiumServer = "http://127.0.0.1:4723/";
 		URL appiumServerUrl = new URL(urlOfAppiumServer);
 		UiAutomator2Options options=new UiAutomator2Options();
+		//Setting the device name
 		options.setDeviceName("Pixel2");
+		//Setting app location
 		options.setApp("/Users/apple/eclipse-workspace/MDITTrainings/MDIT2023/appium/src/test/resources/ApiDemos-debug.apk");
 		
+		//Launching the app
 		AndroidDriver driver=new AndroidDriver(appiumServerUrl,options);
+		//Implicitly waiting for 10 seconds for tester to see the app launch
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//Quitting the driver
 		driver.quit();
 		
 	}
